19fbb9eeddc30a6d8cb8ecdaeef0e513fb8aae3c
LuticaCANARD <presan100@gmail.com>
2022-04-24 18:18:30 +0900

    set get

---
+++ src/routes/index.svelte
@@ -0,0 +1,16 @@
+<script>
+    import {browser} from '$app/env'
+</script>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+{#if browser}
+<p1> {browser}</p1>
+{:else}
+<script>
+    let veo =""
+</script>
+{/if}
+<p1> ? </p1>
+
+
